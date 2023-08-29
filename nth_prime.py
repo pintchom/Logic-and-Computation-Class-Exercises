32a74834d0caa4d838c13672c1da6847f91370b0
pintchom <124115015+pintchom@users.noreply.github.com>
2023-08-29 18:29:52 -0400

    question 6 changes

--- nth_prime.py
+++ nth_prime.py
@@ -1,14 +1,14 @@
 
 def nth_prime(n):
-    primes = [2]
-    curNum = 2
+    primes = [2] #Minimum prime numbers to check for is 1 and 2 will always be the first prime number
+    curNum = 2 #Starting the next from here
     while len(primes) < n:
         curNum += 1
-        prime = True
+        prime = True # determines whether added to primes output 
         for num in primes:
-            if int((curNum/num)) == curNum/num:
+            if int((curNum/num)) == curNum/num: #if int(curNum/Num) == float(curNum/NUum)
                 prime = False
-                break 
+                break # if this ever works, definitely not a prime number
         if prime:
             primes.append(curNum)
     return primes[-1]
@@ -18,6 +18,6 @@ def nth_prime(n):
 if __name__ == "__main__":
     while True:
         userInput = int(input('> '))
-        if userInput == 0:
+        if userInput == 0: #program stops when 0 inputted or program stopped
             break
         print(nth_prime(userInput))
