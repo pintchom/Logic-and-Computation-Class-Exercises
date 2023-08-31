64ed8009a6f4660fea7135a24afe3cc9d5e765e0
pintchom <124115015+pintchom@users.noreply.github.com>
2023-08-31 10:43:47 -0400

    Chaneges 4 and 6

--- nth_prime.py
+++ nth_prime.py
@@ -1,20 +1,30 @@
+def is_prime(n):
+
+    if n < 2:
+        return False
+    curNum = 2
+
+
+    while curNum ** 2 <= n: 
+    # Realized that if a number is prime, it's factors will lie below the first factor squared to be greater than n 
+
+        if n % curNum == 0: # if curnum is a factor, then n is def not prime 
+            return False 
+        curNum += 1
+
+    return True # Runtime O( sqrt(n) ), Memory O(1) 
+
 def nth_prime(n):
-        
+    
     primes = [2]
     curNum = 2
 
     while len(primes) < n:
 
-        add = True
         curNum += 1
+        if is_prime(curNum): primes.append(curNum)
 
-        for prime in primes:
-            if curNum%prime == 0:
-                add = False
-                break
-
-        if add: primes.append(curNum)
-
+    print(primes)
     return primes[-1]
 
 if __name__ == "__main__":
