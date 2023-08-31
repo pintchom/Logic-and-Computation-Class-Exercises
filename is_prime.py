03e6d7bf1bdd98443a0521086a4fb68cc1f044cb
pintchom <124115015+pintchom@users.noreply.github.com>
2023-08-31 10:21:20 -0400

    Working Q4 and Q6

--- is_prime.py
+++ is_prime.py
@@ -1,24 +1,16 @@
-
-
 def is_prime(n):
-    primes = [2]
-    curNum = 2
-    while curNum < n:
-        curNum += 1
-        add = True 
-        for prime in primes:
-            if int((curNum/prime)) == curNum/prime: #if int(curNum/Num) == float(curNum/NUum)
-                add = False
-                break
-        if add: primes.append(curNum)
-    if primes[-1] == n: return True 
-    return False
-
+    
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n%i==0:
+            return False 
+    return True 
+    
 
 if __name__ == "__main__":
-    print("\n> Enter -1 to quit <")
+
     while True:
         userInput = int(input('> '))
-        if userInput == -1: break
         print(is_prime(userInput))
 
--- nth_prime.py
+++ nth_prime.py
@@ -1,24 +1,24 @@
-
 def nth_prime(n):
-    primes = [2] #Minimum prime numbers to check for is 1 and 2 will always be the first prime number
-    curNum = 2 #Starting the next from here
-    while len(primes) < n:
-        curNum += 1
-        prime = True # determines whether added to primes output 
-        for num in primes:
-            if int((curNum/num)) == curNum/num: #if int(curNum/Num) == float(curNum/NUum)
-                prime = False
-                break # if this ever works, definitely not a prime number
-        if prime:
-            primes.append(curNum)
-    print(primes)
-    return primes[-1]
         
+    primes = [2]
+    curNum = 2
 
-        
+    while len(primes) < n:
+
+        add = True
+        curNum += 1
+
+        for prime in primes:
+            if curNum%prime == 0:
+                add = False
+                break
+
+        if add: primes.append(curNum)
+
+    return primes[-1]
+
 if __name__ == "__main__":
+        
     while True:
         userInput = int(input('> '))
-        if userInput == 0: #program stops when 0 inputted or program stopped
-            break
         print(nth_prime(userInput))
